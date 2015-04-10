fd58898dddd043ce02a1f02adae5f0752e1e34c4
butterandfly <zero.hero.lin@gmail.com>
2015-04-10 17:04:55 +0800

    添加了macbook上的内容

--- entry.sh
+++ entry.sh
@@ -2,3 +2,8 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
 source ${BASEDIR}/env.sh;
 source ${BASEDIR}/program_alias.sh;
+
+# PS1
+export PS1="^_^凸 \u@\h (\W)$ "
+
+echo 'Welcome back, Zero.'
--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 echo Setting env...
 
 export PATH=$PATH:~/.bin
+# 历史记录增容
+export HISTSIZE=200000
 
 echo Setting env finished!
